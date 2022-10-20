027bcc2b25cae4f76839cf4389bc5e140c97ce0f
Stephen Power <stephenxpower2003@gmail.com>
2022-10-20 12:00:42 +0100

    Change site header.

--- src/index.js
+++ src/index.js
@@ -6,18 +6,12 @@ import MoviePage from "./pages/movieDetailsPage";
 import favouriteMoviesPage from "./pages/favouriteMoviesPage"; 
 import {Link} from 'react-router-dom'
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favourites">Favourites</Link>
-        </li>
-      </ul>
+     <SiteHeader />
       <Routes>
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route exact path="/movies/favourites" element={<favouriteMoviesPage />} />
