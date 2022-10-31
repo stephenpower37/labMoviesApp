3d1e840f181ce398d80637b30cd875e3a50c9804
Stephen Power <stephenxpower2003@gmail.com>
2022-10-31 11:35:19 +0000

    Fix upcoming movies page.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -73,13 +73,16 @@ export const getMovie = (args) => {
       });
   };
 
-  export const getUpcomingMovies = () => {
+  export const getUpcomingMovies = async () => {
     return fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json.results);
-        return json.results;
-      });
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
   };
